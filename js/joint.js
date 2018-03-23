cebc9e50e7da3877f7e636180b02bd99d7f0c19b
Potrjas <potrjas@ukr.net>
2018-03-23 19:21:24 +0200

    add "Core values"

--- js/joint.js
+++ js/joint.js
@@ -2,40 +2,26 @@ $(document).ready(function() {
     $(window).scroll(function() {
         if ($(this).scrollTop() > 100 && $(this).width() > 799) {
             $('header').css({
-                'backgroundColor': 'rgba(0, 0, 0, 0.9)',
+                'background-color': 'rgba(0, 0, 0, 0.9)',
                 'color': '#fff'
             });
             $('#logo').css('left', '0');
-            $('header div.fa').css({
-                'color': '#fff'
-            });
-            $('header div.fa img').css({
-                'filter': 'invert(1)',
-                '-webkit-filter': 'invert(1)'
-            });
         }
         else if ($(this).scrollTop() <= 100 && $(this).width() > 799) {
             $('header').css({
-                'backgroundColor': '',
+                'background-color': '',
                 'color': ''
             });
             $('#logo').css('left', '');
-            $('header div.fa').css({
-                'color': ''
-            });
-            $('header div.fa img').css({
-                'filter': '',
-                '-webkit-filter': ''
-            });
         }
     });
     $('#nav').click(function() {
         if ($('nav').is(':hidden')) {
-            $('nav, header div.fa').slideDown(300);
+            $('nav').slideDown(300);
             $(this).html('&#xf00d;');
         }
         else {
-            $('nav, header div.fa').slideUp(300);
+            $('nav').slideUp(300);
             $(this).html('&#xf0c9;');
         }
     });
@@ -53,19 +39,16 @@ $(document).ready(function() {
         top = $(id).offset().top - 100;
         if ($(window).width() < 800) {
             topMenu.slideUp(300);
-            $('header div.fa').slideUp(300);
             $('#nav').html('&#xf0c9;');
         }
         $('body, html').animate({scrollTop: top + 32}, 500);
     });
     if ($(window).width() < 800) {
-        $('#logo, header .fa a').click(function() {
+        $('#logo').click(function() {
             topMenu.slideUp(300);
-            $('header div.fa').slideUp(300);
             $('#nav').html('&#xf0c9;');
         });
     }
-
     $(window).scroll(function() {
         var fromTop = $(this).scrollTop() + topMenuHeight;
         var cur = scrollItems.map(function() {if ($(this).offset().top < fromTop) return this;});
